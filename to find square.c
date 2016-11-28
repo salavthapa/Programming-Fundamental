ddfe51f61298f80824bcb3caaba17b98bdbdcd0a
salavthapa <salav.thapa4@gmail.com>
2016-11-28 12:29:24 +0545

    Add files via upload

---
+++ to find square.c
@@ -0,0 +1,26 @@
+/*
+Student Name:Salav Thapa
+Subject:Programing Fundamental
+Roll No:
+Program:To enter any number and calculate its square
+Lab Sheet No:07
+Date:2016-11-28
+*/
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float a,b;
+    printf("Enter the value of a:\n");
+    scanf("%f",&a);
+    b=pow(a,2);
+    printf("the value of a: %f",b);
+    return 0;
+}
+
+
+
+
+
+
+
