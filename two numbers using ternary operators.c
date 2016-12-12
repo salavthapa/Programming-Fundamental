58834e24922c627b01fa5f394bf8c5ed52aed13f
salavthapa <salav.thapa4@gmail.com>
2016-12-12 11:39:21 +0545

    Add files via upload

---
+++ two numbers using ternary operators.c
@@ -0,0 +1,22 @@
+/*
+Student Name:SALAV THAPA
+Subject:Programing Fundamental
+Roll No:30
+Program:to print find largest of tow numbers using ternary operators
+Date:2016-12-12
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a,b;
+    printf("Enter the number in a:\n");
+    scanf("%d",&a);
+    printf("Enter the number of b:\n");
+    scanf("%d",&b);
+    (a>=b)? printf("a is the greater"):
+            printf("b is greater");
+     getch();
+     return 0;
+
+}
