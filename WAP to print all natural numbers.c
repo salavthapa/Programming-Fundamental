68108ce71aac1c978c956df1903c0ffb921ce36a
salavthapa <salav.thapa4@gmail.com>
2017-01-08 13:04:26 +0545

    Add files via upload

---
+++ WAP to print all natural numbers.c
@@ -0,0 +1,30 @@
+/*
+Student Name:SALAV THAPA
+Program:Programming fundamental
+Roll No:
+Program:WAP to print all natural numbers
+Lab Sheet No:16
+Date:2017-01-08
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0,n;
+    printf("Enter your number\n");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("%d\t",i);
+        i++;
+    }
+    getch();
+    return 0;
+}
+
+
+
+
+
+
+
