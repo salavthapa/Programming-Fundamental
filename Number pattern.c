ded40fbf8e3288f8163dff78c424935132dde05b
salavthapa <salav.thapa4@gmail.com>
2017-01-15 10:20:33 +0545

    Add files via upload

---
+++ Number pattern.c
@@ -0,0 +1,26 @@
+/*
+Student Name:salav Thapa
+subject:Programing Fundamental
+Lab Sheet No:18
+Program:WAP to display number pattern
+Roll No:30
+Date:15 jan 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+
+}
+
---
+++ to print star.c
@@ -0,0 +1,24 @@
+/*
+Student Name:salav Thapa
+subject:Programing Fundamental
+Lab Sheet No:18
+Program:WAP to print star
+Roll No:30
+Date:15 jan 2016
+*/
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
